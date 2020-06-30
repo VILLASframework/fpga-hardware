294c924e5bfb8c7524ebeea06b671ccd4a69dbdf
Hatim Kanchwala <hatim@hatimak.me>
2020-06-30 13:28:55 +0200

    Update scripts, streamline Makefile

--- vc707/vc707_villas/scripts/bitstream.tcl
+++ vc707/vc707_villas/scripts/bitstream.tcl
@@ -1,9 +1,6 @@
 # Get the directory where this script resides
 set thisDir [file dirname [info script]]
 
-# Source common utilities
-source -notrace $thisDir/utils.tcl
-
 open_project ./vivado/vc707_villas/vc707_villas.xpr
 
 reset_run synth_1
@@ -18,5 +15,3 @@ write_bitstream -verbose ./build/vc707_villas.bit -force
 # Generate configuration memory for VC707 BPI flash mt28gu01gaax1e-bpi-x16
 write_cfgmem -format mcs -size 128 -interface BPIx16 -loadbit {up 0x00000000 "./build/vc707_villas.bit" } -force -file "./build/vc707_villas.mcs"
 
-# Generate Hardware Definition file
-write_hwdef -force -file "./build/vc707_villas.hdf"
--- vc707/vc707_villas/scripts/hwdef.tcl
+++ vc707/vc707_villas/scripts/hwdef.tcl
@@ -1,4 +1,8 @@
+# Get the directory where this script resides
+set thisDir [file dirname [info script]]
+
 open_project ./vivado/vc707_villas/vc707_villas.xpr
 
 # Generate Hardware Definition file
 write_hwdef -force -file "./build/vc707_villas.hdf"
+
